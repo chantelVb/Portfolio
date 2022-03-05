08b6bb1e53e4ddd0aedce0f22bba52fc958b2302
Chantel Boiro <97921215+chantelVb@users.noreply.github.com>
2022-03-05 20:44:48 +0000

    added Dice Game

---
+++ My-Dice-Game/index.js
@@ -0,0 +1,32 @@
+// Player one
+var pickRandomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage = "dice" + pickRandomNumber1 + ".png";
+
+var imageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", imageSource);
+
+// Player 2
+
+var pickRandomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage = "dice" + pickRandomNumber2 + ".png";
+
+var imageSource = "images/" + randomDiceImage;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", imageSource);
+
+// win or draw message
+
+if (pickRandomNumber1 > pickRandomNumber2){
+    document.querySelector("h1").innerHTML = "🏆 You Win Player 1";
+}if (pickRandomNumber1 < pickRandomNumber2){
+    document.querySelector("h1").innerHTML = "You Win Player 2 🏆";
+}else if(pickRandomNumber1===pickRandomNumber2){
+    document.querySelector("h1").innerHTML = "Its a Draw 🎲"
+}
